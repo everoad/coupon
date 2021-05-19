036d5bbd30d1f9d83e251fe914f36ed5a25c2add
everaod <everoad102@gmail.com>
2021-05-20 01:20:49 +0900

    CouponProvider 수정

--- backend/src/main/java/com/smilegate/coupon/backend/core/provider/CouponProvider.java
+++ backend/src/main/java/com/smilegate/coupon/backend/core/provider/CouponProvider.java
@@ -23,25 +23,47 @@ public class CouponProvider {
 
     public String createCode(String phoneNumber) {
         SecureRandom random = new SecureRandom();
-        byte[] randomBytes = new byte[6];
+        byte[] randomBytes = new byte[10];
         String seed = System.currentTimeMillis() + phoneNumber;
         random.setSeed(seed.getBytes(StandardCharsets.UTF_8));
         random.nextBytes(randomBytes);
-        return convertByteArrayToBase32(randomBytes);
+        return byteArrayToBase32(randomBytes);
     }
 
-    private String convertByteArrayToBase32(byte[] bytes) {
-        StringBuilder builder = new StringBuilder();
-        for (final byte b : bytes) {
-            String str = Integer.toString(b & 0xff, 32);
-            str = Math.round(Math.random()) > 0 ? str.toUpperCase() : str.toLowerCase();
-            if (str.length() < 2) {
-                builder.append("0").append(str);
-            } else {
-                builder.append(str);
-            }
+    public String byteArrayToBase32(byte[] data) {
+        StringBuilder result = new StringBuilder();
+        byte[] bits = new byte[data.length * 8];
+        for (int i = 0; i < data.length; i++) {
+            bits[i * 8] = (byte) ((data[i] & 0x80) >> 7);
+            bits[i * 8 + 1] = (byte) ((data[i] & 0x40) >> 6);
+            bits[i * 8 + 2] = (byte) ((data[i] & 0x20) >> 5);
+            bits[i * 8 + 3] = (byte) ((data[i] & 0x10) >> 4);
+            bits[i * 8 + 4] = (byte) ((data[i] & 0x08) >> 3);
+            bits[i * 8 + 5] = (byte) ((data[i] & 0x04) >> 2);
+            bits[i * 8 + 6] = (byte) ((data[i] & 0x02) >> 1);
+            bits[i * 8 + 7] = (byte) ((data[i] & 0x01));
         }
-        return builder.toString();
+        for (int i = 0; i < data.length / 5 * 8 - 4; i++) {
+            boolean isUpper = Math.round(Math.random()) > 0;
+            String str = "";
+            byte value = (byte) (bits[i * 5] << 4
+                    | bits[i * 5 + 1] << 3 | bits[i * 5 + 2] << 2
+                    | bits[i * 5 + 3] << 1 | bits[i * 5 + 4]);
+            if (value >= 0 && value < 26) {
+                str += (char) (value + 'A');
+            }
+            if (value >= 26 && value < 30) {
+                str += (char) (value - 26 + '2');
+            }
+            if (value == 30) {
+                str += '7';
+            }
+            if (value == 31) {
+                str += '9';
+            }
+            result.append(isUpper ? str.toUpperCase() : str.toLowerCase());
+        }
+        return result.toString();
     }
 
 }
